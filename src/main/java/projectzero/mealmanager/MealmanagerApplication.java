63f34de0dbe9972a9641e219ce34e83ae3f7c807
Angelo <angelo.alfano@accenture.com>
2019-04-02 00:06:28 +0200

    Created base project structure.

---
+++ src/main/java/projectzero/mealmanager/MealmanagerApplication.java
@@ -0,0 +1,13 @@
+package projectzero.mealmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealmanagerApplication.class, args);
+	}
+
+}
